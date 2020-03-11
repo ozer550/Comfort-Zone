6946a9ec26a6a2c57705ab31ef2da43a56d5113d
ozer619 <prathameshd419@gmail.com>
2020-03-11 12:52:12 +0530

    layout

--- application.py
+++ application.py
@@ -8,3 +8,7 @@ app = flask.Flask(__name__)
 @app.route("/")
 def index():
     return flask.render_template("index.html")
+
+@app.route("/Label")
+def Label():
+    return flask.render_template("label.html")
